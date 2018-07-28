2091873e6b5ec6294cafb12c2e17e3edea6ca441
James Tombleson <luther38@gmail.com>
2018-07-27 21:31:01 -0700

    Removed Snaps

--- Shell/baseline.sh
+++ Shell/baseline.sh
@@ -20,6 +20,37 @@ sudo dpkg -i mongodb-compass-community_1.14.6_amd64.deb
 sudo apt --fix-broken install
 rm mongodb-compass-community_1.14.6_amd64.deb
 
+# Google Chrome
+# I am use to this :V
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+sudo apt --fix-broken install
+rm discord.deb
+
+# Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+sudo apt --fix-broken install
+rm gitkraken-amd64.deb
+
+# Signal Desktop
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# Visual Studio Code
+wget -O vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
+sudo dpkg -i vscode.deb
+rm vscode.deb
+
 #varitey - wallpaper switcher
 sudo add-apt-repository ppa:peterlevi/ppa -y
 
@@ -29,6 +60,7 @@ sudo add-apt-repository -u ppa:snwh/ppa -y
 
 sudo apt-get update
 
+sudo apt-get install curl -y
 sudo apt-get install snapd -y 
 sudo apt-get install node.js -y 
 sudo apt-get install npm -y
@@ -38,6 +70,9 @@ sudo apt-get install gnome-tweaks -y
 sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons 
 sudo apt-get install dconf-tools -y
 sudo apt-get install gimp -y
+sudo apt-get install spotify-client -y
+sudo apt-get install docker-compose -y
+sudo apt-get install signal-desktop -y
 
 # Adjust Ubuntu Dock
 # https://linuxconfig.org/how-to-customize-dock-panel-on-ubuntu-18-04-bionic-beaver-linux
@@ -52,7 +87,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
 
 #Snaps are built to auto update
 #might move docker to apt
-sudo snap install discord spotify docker cpufreq
+sudo snap install cpufreqd
 
 # cpuFreq Gnome Extension
 # https://extensions.gnome.org/extension/1082/cpufreq/
